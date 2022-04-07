ad765e89dca53f577595c4ac2882e5876b95f435
Isaac Fonseca <isaacf8822@gmail.com>
2022-04-07 15:01:09 -0500

    commit tree c7058f21c03bc6aa45125316dd501e207740f14c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, event) {
+    let newCards = [];
+    for (let i=0; i<names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        newCards.push(message);
+    }
+    return newCards;
+}
+
+writeCards(names);
+
+function countDown(int) {
+    let i=10;
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    }
+    return int
+}
